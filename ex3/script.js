c79a321a7592629f937dd3676cb27bfda76fa54c
coletucker22 <59104432+coletucker22@users.noreply.github.com>
2020-03-10 19:08:00 -0500

    Completed Excersise 3

--- ex3/script.js
+++ ex3/script.js
@@ -3,5 +3,12 @@
 var button = document.getElementById("myButton");
 
 button.addEventListener('click', function(){
-    
+    const curColour = document.getElementById('box').style.backgroundColor;
+
+    if (curColour === 'red') {
+        document.getElementById('box').style.backgroundColor = "blue";
+    }
+    else {
+        document.getElementById('box').style.backgroundColor = "red";
+    }
 })
